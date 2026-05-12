9fbb6cfa52c2b49c1d69094aba12ac71a81ad058
ChaitanyaPalve <palwe.chaitanya@gmail.com>
2026-05-12 23:48:29 +0530

    Completed blockchain cloud file sharing system

--- Frontend/script.js
+++ Frontend/script.js
@@ -7,72 +7,101 @@ import {
 } from "./firebase.js";
 
 window.register = async function () {
+
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
-  await registerUser(email, password);
-  alert("Registered successfully");
+  try{
+    await registerUser(email, password);
+    alert("Registered Successfully");
+  }
+  catch(error){
+    alert(error.message);
+  }
 };
 
 window.login = async function () {
+
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
-  await loginUser(email, password);
-  alert("Logged in successfully");
+  try{
+    await loginUser(email, password);
+    alert("Login Successful");
+  }
+  catch(error){
+    alert(error.message);
+  }
 };
 
 window.logout = async function () {
-  await logoutUser();
-  alert("Logged out");
+
+  try{
+    await logoutUser();
+    alert("Logged Out");
+  }
+  catch(error){
+    alert(error.message);
+  }
 };
 
 window.uploadFile = async function () {
+
   const fileInput = document.getElementById("fileInput");
   const result = document.getElementById("result");
   const fileTable = document.getElementById("fileTable");
 
-  if (!auth.currentUser) {
+  if(!auth.currentUser){
     result.innerHTML = "Please login first.";
     return;
   }
 
-  if (!fileInput.files.length) {
-    result.innerHTML = "Please select a file first.";
+  if(!fileInput.files.length){
+    result.innerHTML = "Please select a file.";
     return;
   }
 
   const file = fileInput.files[0];
+
   result.innerHTML = "Uploading file...";
 
   const formData = new FormData();
   formData.append("file", file);
 
-  const response = await fetch("http://127.0.0.1:8000/upload", {
-    method: "POST",
-    body: formData
-  });
+  try{
 
-  const data = await response.json();
+    const response = await fetch("http://127.0.0.1:8000/upload",{
+      method:"POST",
+      body:formData
+    });
 
-  await saveFileRecord({
-    filename: file.name,
-    cid: data.cid,
-    ipfs_url: data.ipfs_url,
-    owner_email: auth.currentUser.email
-  });
+    const data = await response.json();
 
-  result.innerHTML = `
-    File uploaded successfully!<br>
-    CID: ${data.cid}<br>
-    <a href="${data.ipfs_url}" target="_blank">Open IPFS File</a>
-  `;
+    await saveFileRecord({
+      filename:file.name,
+      cid:data.cid,
+      ipfs_url:data.ipfs_url,
+      owner_email:auth.currentUser.email
+    });
 
-  fileTable.innerHTML += `
-    <tr>
-      <td>${file.name}</td>
-      <td>${data.cid}</td>
-      <td>Saved in Firebase + IPFS</td>
-    </tr>
-  `;
+    result.innerHTML = `
+      File uploaded successfully!<br><br>
+      CID: ${data.cid}<br><br>
+      <a href="${data.ipfs_url}" target="_blank">
+        Open IPFS File
+      </a>
+    `;
+
+    fileTable.innerHTML += `
+      <tr>
+        <td>${file.name}</td>
+        <td>${data.cid}</td>
+        <td>Stored Successfully</td>
+      </tr>
+    `;
+
+  }
+  catch(error){
+    result.innerHTML = error.message;
+  }
 };
--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI, UploadFile, File, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
+from web3 import Web3
 import requests
 import os
+import json
 
 load_dotenv()
 
@@ -17,6 +19,29 @@ app.add_middleware(
 )
 
 PINATA_JWT = os.getenv("PINATA_JWT")
+
+# Blockchain Connection
+w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
+
+contract_address = Web3.to_checksum_address(
+    "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+)
+
+wallet_address = Web3.to_checksum_address(
+    "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266"
+)
+
+private_key = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+
+with open("../blockchain/artifacts/contracts/FileStorage.sol/FileStorage.json") as f:
+    contract_json = json.load(f)
+
+contract_abi = contract_json["abi"]
+
+contract = w3.eth.contract(
+    address=contract_address,
+    abi=contract_abi
+)
 
 
 @app.get("/")
@@ -69,8 +94,33 @@ async def upload_file(file: UploadFile = File(...)):
 
     result = response.json()
 
+    # Store CID on Blockchain
+    nonce = w3.eth.get_transaction_count(wallet_address)
+
+    txn = contract.functions.addFile(
+        file.filename,
+        result["IpfsHash"]
+    ).build_transaction({
+        "chainId": 31337,
+        "gas": 2000000,
+        "gasPrice": w3.to_wei("1", "gwei"),
+        "nonce": nonce,
+    })
+
+    signed_txn = w3.eth.account.sign_transaction(
+        txn,
+        private_key=private_key
+    )
+
+    tx_hash = w3.eth.send_raw_transaction(
+        signed_txn.raw_transaction
+    )
+
+    tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+
     return {
         "filename": file.filename,
         "cid": result["IpfsHash"],
-        "ipfs_url": f"https://gateway.pinata.cloud/ipfs/{result['IpfsHash']}"
+        "ipfs_url": f"https://gateway.pinata.cloud/ipfs/{result['IpfsHash']}",
+        "blockchain_tx": tx_hash.hex()
     }
